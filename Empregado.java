ad029391d8ea63fdaa550776825be47af9b31d3c
Rosa-MarciaOlive <rosa.marciaolive@gmail.com>
2021-10-25 16:18:06 +0000

    Conti.......

--- Empregado.java
+++ Empregado.java
@@ -1,9 +1,26 @@
 public class Empregado extends Pessoa{
-    ///nome endereço não perecisa
+//nome endereço não precisa
     private int codigoSetor;
     private double salarioBase;
     private double imposto;
 
-    //fazer metodo set e get...
-
+    public void setCodigoSetor(int codigoSetor){
+    this.codigoSetor=godigoSetor;
+    }
+    //get
+    public String getgodigoSetor(){
+    return this.godigoSetor;
+    }
+    public void setSalarioBase(double salarioBase){
+        this.salarioBase=salarioBase;
+    }
+    public getsalarioBase(){
+        return this.salarioBase;
+    }
+    public void setImposto(double imposto){
+        this.imposto=imposto;
+    }
+    public getimposto(){
+        return this.imposto;
+    }
 }
---
+++ Principal.java
@@ -0,0 +1,23 @@
+public class Principal{
+    public static void main(String[]args){
+//Acionar o arquivo Pessoar por aqui:
+        Pessoa p=new Pessoa();
+        p.setNome();
+        p.setTelefone();
+        p.setEndereço ();
+//Selecionar cliente ao digitar seu codigo usando if e return......falta
+        System.out.println('Dados do cliente: ',)
+
+//Acionar a pasta arquivo:
+        Empregado e=new Empregado();
+        e.setCodigoSetor();
+        e.setSalarioBase();
+        e.setImposto();
+//Acessar com código individual de cada empregado mas primeiro escolhendo
+//  o setor, imagino com if ou while 
+        System.out.println('')
+
+
+
+    }
+}
