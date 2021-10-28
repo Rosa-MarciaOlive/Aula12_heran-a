4b01908728841946c5e257ef96c15814a0c9866b
Rosa-MarciaOlive <rosa.marciaolive@gmail.com>
2021-10-28 04:24:41 +0000

    Empregado ok!

--- Empregado.java
+++ Empregado.java
@@ -1,22 +1,29 @@
 public class Empregado extends Pessoa{
+
+//codigoSetor (inteiro), salarioBase (vencimento base) e imposto
+//(porcentagem retida dos impostos)
+
 //nome endereço não precisa
     private int codigoSetor;
     private double salarioBase;
     private double imposto;
 
+//set-->Método de alteração
     public void setCodigoSetor(int codigoSetor){
     this.codigoSetor=godigoSetor;
     }
-    //get
+//get-->Métode de consulta
     public String getgodigoSetor(){
     return this.godigoSetor;
     }
+
     public void setSalarioBase(double salarioBase){
         this.salarioBase=salarioBase;
     }
     public getsalarioBase(){
         return this.salarioBase;
     }
+
     public void setImposto(double imposto){
         this.imposto=imposto;
     }
